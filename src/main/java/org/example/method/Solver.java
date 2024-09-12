d4af727e1758620c6a9057b4d78f3501432a3779
Akosse <akossedevamp@gmail.com>
2024-09-12 21:16:31 +0100

    commit message

--- src/main/java/org/example/method/Solver.java
+++ src/main/java/org/example/method/Solver.java
@@ -4,11 +4,20 @@ import java.util.ArrayList;
 
 public class Solver {
     public static ProjectileMethod numberOfIterations(int iterations, double endHeight, ProjectileMethod projectileMethod, double timeStep) throws Exception {
-        double
+        ArrayList<Double> currentHeight;
+        boolean isGoingDown = false;
+        ArrayList<Double> currentVelocity;
         for(int i = 0; i < iterations; i++){
             projectileMethod.nextStep(timeStep);
-
-
+            currentVelocity = projectileMethod.getProjectile().getVelocity();
+            currentHeight = projectileMethod.getProjectile().getDisplacement();
+            if(currentVelocity.get(1) < 0){
+                isGoingDown = true;
+            }
+            if((currentHeight.get(1) <= endHeight) && isGoingDown){
+                return projectileMethod;
+            }
         }
+        return projectileMethod;
     }
 }
