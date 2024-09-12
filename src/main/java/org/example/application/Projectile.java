90c8b342c886fc1e074ae9ef90e2c1540e7016b3
Akosse <akossedevamp@gmail.com>
2024-09-12 21:59:18 +0100

    commit message

--- src/main/java/org/example/application/Projectile.java
+++ src/main/java/org/example/application/Projectile.java
@@ -15,6 +15,7 @@ public class Projectile {
     private ArrayList<Double> acceleration;
     private double dragCoefficient = 0.3; //Previous 0.0579
     // use this data to calculate a better cD https://core.ac.uk/download/pdf/333722658.pdf
+    // Mach number at altitudes https://aerospaceweb.org/question/atmosphere/q0112.shtml
     private double crossSectionalArea = Math.PI * ((0.155 * 0.155) / 4);
     private double mass = 43;
     // Using HE M101 155mm artillery shell as reference for now
